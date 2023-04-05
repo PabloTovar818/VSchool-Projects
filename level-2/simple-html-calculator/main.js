b37a38f852bd86ebed7c95ba973146862846724d
Pablo Tovar <pablorodrigotova@gmail.com>
2023-04-04 22:10:55 -0400

    completed assignment

--- level-2/simple-html-calculator/main.js
+++ level-2/simple-html-calculator/main.js
@@ -1,16 +1,28 @@
 let h3 = document.createElement('h3');
-let num1 = document.myForm.addContainer.addNum1.value;
-console.log(num1);
+let result = 0;
 
 const addBtn = document.querySelector('#add-btn').addEventListener('click', e => {
     e.preventDefault();
-    let num1 = document.getElementsByName('add-num2').value;
-    let num2 = document.getElementsByName('add-num2').value;
-
-    let result = num1 + num2;
+    let num1 = document.getElementById('add-num1').value;
+    let num2 = document.getElementById('add-num2').value;
+    result = parseInt(num1) + parseInt(num2);
     h3.textContent = result;
     document.body.append(h3);
 
 });
-const subBtn = document.querySelector('#sub-btn');
-const mulBtn = document.querySelector('#mul-btn');
+const subBtn = document.querySelector('#sub-btn').addEventListener('click', e => {
+    e.preventDefault();
+    let num1 = document.getElementById('sub-num1').value;
+    let num2 = document.getElementById('sub-num2').value;
+    result = parseInt(num1) - parseInt(num2);
+    h3.textContent = result;
+    document.body.append(h3);
+});
+const mulBtn = document.querySelector('#mul-btn').addEventListener('click', e => {
+    e.preventDefault();
+    let num1 = document.getElementById('mul-num1').value;
+    let num2 = document.getElementById('mul-num2').value;
+    result = parseInt(num1) * parseInt(num2);
+    h3.textContent = result;
+    document.body.append(h3);
+});
