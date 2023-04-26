6ad1473b86fe864154289350c828c1c91361feda
Pablo Tovar <pablorodrigotova@gmail.com>
2023-04-26 17:32:10 -0400

    modified content

--- level-2/axios-practice/app.js
+++ level-2/axios-practice/app.js
@@ -1,14 +1,40 @@
-axios.get("https://api.vschool.io/pablo/todo")
-    .then(response => {
-        for (let i = 0; i < response.data.length; i++) {
-            const h1 = document.createElement("h1");
-            const p = document.createElement("p");
-            h1.textContent = response.data[i].title;
-            p.textContent = response.data[i].description;
-            document.body.appendChild(h1);
-            document.body.appendChild(p);
-        }
-    })
-    .catch(error => {
-        console.log(error);
-    })
+function getRequest() {
+    axios.get("https://api.vschool.io/pablo/todo")
+        .then(response => listData(response.data))
+        .catch(error => console.log(error))
+}
+
+function listData(data) {
+    clearList();
+    const todoList = document.querySelector("#todo-list")
+    for (let i = 0; i < data.length; i++) {
+        const h1 = document.createElement("h1");
+        h1.textContent = data[i].title;
+        todoList.appendChild(h1);
+    }
+}
+
+function clearList() {
+    const el = document.querySelector("#todo-list");
+    while(el.firstChild) {
+        el.removeChild(el.firstChild);
+    }
+}
+
+function postRequest(value) {
+    axios.post("https://api.vschool.io/pablo/todo", value)
+       .then(response => listData(response.data))
+       .catch(error => console.log(error))
+}
+
+getRequest();
+
+const form = document.todoForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const newItem = {
+        title: todoForm.title.value
+    }
+    postRequest(newItem);
+});
+
+const refreshButton = document.querySelector("#refresh-button").addEventListener("click", getRequest);
