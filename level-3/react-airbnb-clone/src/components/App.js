08b2b592db6ab6383d3d49e845992273fd45bacb
Pablo Tovar <pablorodrigotova@gmail.com>
2023-05-09 12:56:10 -0400

    added props to Card component, style changes

--- level-3/react-airbnb-clone/src/components/App.js
+++ level-3/react-airbnb-clone/src/components/App.js
@@ -2,14 +2,38 @@ import React from "react"
 import Navbar from "./Navbar"
 import Hero from "./Hero"
 import Card from "./Card"
-//import other components for structuring below
+import katieZaferesPhoto from "../img/card-images/image-12.png"
+import weddingPhoto from "../img/card-images/wedding-photography-1.png"
+import bikePhoto from "../img/card-images/mountain-bike-1.png"
 
 export default function App() {
     return (
         <div>
             <Navbar />
             <Hero />
-            <Card />
+            <div className="card-container">
+                <Card 
+                    img={katieZaferesPhoto}
+                    imgAlt="life lessons with Katie Zaferes"
+                    rating="5.0 (6) • USA"
+                    title="Life lessons with Katie Zaferes"
+                    price="From $136"
+                />
+                <Card 
+                    img={weddingPhoto}
+                    imgAlt="learn wedding photography"
+                    rating="5.0 (30) • USA"
+                    title="Learn wedding photography"
+                    price="From $125"
+                />
+                <Card 
+                    img={bikePhoto}
+                    imgAlt="group mountain biking"
+                    rating="4.8 (2) • USA"
+                    title="Group Mountain Biking"
+                    price="From $50"
+                />
+            </div>
         </div>
     )
 }
--- level-3/react-airbnb-clone/src/components/Card.js
+++ level-3/react-airbnb-clone/src/components/Card.js
@@ -1,46 +1,20 @@
 import React from "react"
 import star from "../img/star.png"
-import katieZaferesPhoto from "../img/card-images/image-12.png"
-import weddingPhoto from "../img/card-images/wedding-photography-1.png"
-import bikePhoto from "../img/card-images/mountain-bike-1.png"
+// import katieZaferesPhoto from "../img/card-images/image-12.png"
+// import weddingPhoto from "../img/card-images/wedding-photography-1.png"
+// import bikePhoto from "../img/card-images/mountain-bike-1.png"
 
-export default function Card() {
+export default function Card(props) {
     return (
-        <div className="card-container">
-            <div className="card">
-                <img src={katieZaferesPhoto} alt="life lessons with Katie Zaferes" className="card--image"></img>
-                <div className="card--stats">
-                    <div className="rating">
-                        <img src={star} alt="star" className="rating-image"></img>
-                        <span>5.0 (6) • USA</span>
-                    </div>
-                    <span>Life lessons with Katie Zaferes</span>
-                    <span><bold>From $136</bold> / person</span>
+        <div className="card">
+            <img src={props.img} alt={props.imgAlt} className="card--image"></img>
+            <div className="card--stats">
+                <div className="rating">
+                    <img src={star} alt="star" className="rating-image"></img>
+                    <span>{props.rating}</span>
                 </div>
-            </div>
-
-            <div className="card">
-                <img src={weddingPhoto} alt="learn wedding photography" className="card--image"></img>
-                <div className="card--stats">
-                    <div className="rating">
-                        <img src={star} alt="star" className="rating-image"></img>
-                        <span>5.0 (30) • USA</span>
-                    </div>
-                    <span>Learn wedding photography</span>
-                    <span><bold>From $125</bold> / person</span>
-                </div>
-            </div>
-
-            <div className="card">
-                <img src={bikePhoto} alt="group mountain biking" className="card--image"></img>
-                <div className="card--stats">
-                    <div className="rating">
-                        <img src={star} alt="star" className="rating-image"></img>
-                        <span>4.8 (2) • USA</span>
-                    </div>
-                    <span>Group Mountain Biking</span>
-                    <span><bold>From $50</bold> / person</span>
-                </div>
+                <span>{props.title}</span>
+                <span><bold>{props.price}</bold> / person</span>
             </div>
         </div>
     )
