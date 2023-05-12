e5d3055a3cea1fb21ec5a9aed43a716220b1b68b
Pablo Tovar <pablorodrigotova@gmail.com>
2023-05-12 13:18:17 -0400

    moved card data to data.js file, style changes

--- level-3/react-airbnb-clone/src/components/Card.js
+++ level-3/react-airbnb-clone/src/components/Card.js
@@ -1,8 +1,5 @@
 import React from "react"
 import star from "../img/star.png"
-// import katieZaferesPhoto from "../img/card-images/image-12.png"
-// import weddingPhoto from "../img/card-images/wedding-photography-1.png"
-// import bikePhoto from "../img/card-images/mountain-bike-1.png"
 
 export default function Card(props) {
     return (
@@ -12,6 +9,8 @@ export default function Card(props) {
                 <div className="rating">
                     <img src={star} alt="star" className="rating-image"></img>
                     <span>{props.rating}</span>
+                    <span className="review-count">({props.reviewCount})</span>
+                    <span>{props.location}</span>
                 </div>
                 <span>{props.title}</span>
                 <span><bold>{props.price}</bold> / person</span>
